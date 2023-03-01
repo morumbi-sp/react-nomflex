6561f6660965d9e56d813cd8754a321c016b52aa
Morumbi <zun0323@gmail.com>
2023-03-01 18:47:08 -0300

    add type of api data

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from 'react-query';
-import { getMovies } from '../api';
+import { getMovies, IGetMoviesResult } from '../api';
 
 function Home() {
-  const { isLoading, data } = useQuery(['movies', 'nowPlaying'], getMovies);
+  const { isLoading, data } = useQuery<IGetMoviesResult>(
+    ['movies', 'nowPlaying'],
+    getMovies
+  );
   return <div style={{ backgroundColor: 'white', height: '200vh' }}>home</div>;
 }
 
--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,30 @@
 export const API_KEY = '5d058213508c42de1e97cd1ebdbe63f3';
 
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface IGetMoviesResult {
+  dates: { maximum: string; minimum: string };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export const getMovies = async () => {
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`
