073d9531f59f6d37fafbef379cb32a4d0794cb26
Morumbi <zun0323@gmail.com>
2023-03-01 11:01:26 -0300

    add Header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
-        <Route path={'/'}></Route>
-        <Route path={'/tv'}></Route>
-        <Route path={'/search'}></Route>
+        <Route path={'/'}>
+          <Home />
+        </Route>
+        <Route path={'/tv'}>
+          <Tv />
+        </Route>
+        <Route path={'/search'}>
+          <Search />
+        </Route>
       </Switch>
     </Router>
   );
