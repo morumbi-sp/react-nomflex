a90dc45eb2e9ecf01443eb700f3db0450ff58a30
Morumbi <zun0323@gmail.com>
2023-03-02 10:10:10 -0300

    add back drop image

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,12 +1,59 @@
 import { useQuery } from 'react-query';
+import styled from 'styled-components';
 import { getMovies, IGetMoviesResult } from '../api';
+import { makeImagePath } from '../util';
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 60px;
+  font-weight: 500;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 20px;
+  width: 700px;
+`;
 
 function Home() {
   const { isLoading, data } = useQuery<IGetMoviesResult>(
     ['movies', 'nowPlaying'],
     getMovies
   );
-  return <div style={{ backgroundColor: 'white', height: '200vh' }}>home</div>;
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[1].backdrop_path ?? '')}>
+            <Title>{data?.results[1].title}</Title>
+            <Overview>{data?.results[1].overview}</Overview>
+          </Banner>
+        </>
+      )}{' '}
+    </Wrapper>
+  );
 }
 
 export default Home;
